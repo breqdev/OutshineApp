5fa83a449934bd4d5af907d1d321d23b90eea1a0
Brooke Chalmers <breq@breq.dev>
2021-12-28 20:22:24 -0500

    prototype

--- App.js
+++ App.js
@@ -7,14 +7,95 @@
  */
 
 import React from 'react';
-import {Dimensions, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {ColorWheel} from 'react-native-color-wheel';
+import colorsys from 'colorsys';
+import useStateRef from 'react-usestateref';
+import MODES from './modes';
+import useSerial from './useSerial';
 
 const App = () => {
+  const {connectSerial, writeData} = useSerial();
+
+  const [color, setColor, colorRef] = useStateRef('#ffffff');
+  const [activeMode, setActiveMode, activeModeRef] = useStateRef('01');
+  const dirty = React.useRef(false);
+
+  const handleColorUpdate = React.useCallback(
+    color => {
+      const hex = colorsys.hsv2Hex(color);
+      setColor(hex);
+      dirty.current = true;
+    },
+    [setColor],
+  );
+
+  const handleModeUpdate = React.useCallback(
+    mode => {
+      setActiveMode(mode.code);
+      dirty.current = true;
+    },
+    [setActiveMode],
+  );
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (dirty.current) {
+        const message =
+          colorRef.current.replace('#', '').toUpperCase() +
+          activeModeRef.current;
+        console.log(message);
+        writeData(message);
+        dirty.current = false;
+      }
+    }, 200);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeModeRef, colorRef, writeData]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-      <ColorWheel initialColor="#ee0000" style={styles.wheel} />
+      <Text style={styles.label}>
+        {color} - {activeMode}
+      </Text>
+      <ColorWheel
+        initialColor={color}
+        onColorChange={handleColorUpdate}
+        style={styles.wheel}
+      />
+      <View style={styles.modeButtons}>
+        {MODES.map(mode => (
+          <Pressable
+            style={[
+              styles.button,
+              activeMode === mode.code && styles.activeButton,
+            ]}
+            onPress={() => handleModeUpdate(mode)}
+            key={mode.code}>
+            <Text
+              style={[
+                styles.buttonText,
+                activeMode === mode.code && styles.activeButtonText,
+              ]}>
+              {mode.name}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <View style={styles.configButtons}>
+        <Pressable style={styles.button} onPress={connectSerial}>
+          <Text style={styles.buttonText}>Connect</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
@@ -23,11 +104,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical: 50,
+    backgroundColor: '#000',
+  },
+  label: {
+    fontSize: 20,
+    color: '#fff',
   },
   wheel: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get('window').width * 0.8,
+    height: Dimensions.get('window').width * 0.8,
+  },
+  modeButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  configButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 5,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    borderRadius: 10,
+    width: 80,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  activeButton: {
+    backgroundColor: '#fff',
+  },
+  buttonText: {
+    color: '#fff',
+  },
+  activeButtonText: {
+    color: '#000',
   },
 });
 
---
+++ modes.js
@@ -0,0 +1,44 @@
+const MODES = [
+  {
+    code: '00',
+    name: 'Off',
+  },
+  {
+    code: '01',
+    name: 'Fill',
+  },
+  {
+    code: '13',
+    name: 'Strobe',
+  },
+  {
+    code: '20',
+    name: 'Fade',
+  },
+  {
+    code: '21',
+    name: 'FadWip',
+  },
+  {
+    code: '30',
+    name: 'Wipe',
+  },
+  {
+    code: '32',
+    name: 'Scanner',
+  },
+  {
+    code: '40',
+    name: 'Chase',
+  },
+  {
+    code: '42',
+    name: 'Sparkle',
+  },
+  {
+    code: '50',
+    name: 'Rainbow',
+  },
+];
+
+export default MODES;
